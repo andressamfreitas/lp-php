fc4d1f797687af0018055a48eb06ecf1f6d9ec0c
andressa freitas <andressa.freitas@atlastechnol.com>
2025-05-27 13:24:57 -0300

    feat:add new components

---
+++ code/portfolio/components/projetos.php
@@ -0,0 +1,57 @@
+<div class="bg-slate-800 rounded-lg px-4 py-3 flex items-center">
+                    <div class="w-1/5">Foto do projeto</div>
+                    <div class="w-4/5 space-y-3">
+                        <div class="flex gap-3 justify-between">
+                            <h3 class="font-semibold text-xl">
+                                Projeto 1 <spam class="text-sm opacity-50"> (Finalizado em 2025)</spam></h3>
+                        <div class="flex gap-1 font-semibold text-xs">
+                            <spam class="bg-yellow-500 text-yellow-900 rounded-md px-3 py-2 ">PHP </spam>
+                            <spam class="bg-fuchsia-500 text-fuchsia-900 rounded-md px-3 py-2 ">JS </spam>
+                            <spam class="bg-sky-500 text-sky-900 rounded-md px-3 py-2 ">CSS </spam>
+                            <spam class="bg-lime-500 text-lime-900 rounded-md px-3 py-2 ">Tailwind </spam>
+                        </div>
+                </div>
+                <p class="leading-6">
+                    Lorem ipsum dolor, sit amet consectetur adipiscign elit.
+                    Alias, quas! Nos voluptatem vero dignissimos nemo magnam iusto nam...
+                </p>
+            </section>
+            <section class="space-y-3 py-6"> 
+                <div class="bg-slate-800 rounded-lg px-4 py-3 flex items-center">
+                    <div class="w-1/5">Foto do projeto</div>
+                    <div class="w-4/5 space-y-3">
+                        <div class="flex gap-3 justify-between">
+                            <h3 class="font-semibold text-xl">
+                                Projeto 2 <spam class="text-sm opacity-50"> (Finalizado em 2025)</spam></h3>
+                        <div class="flex gap-1 font-semibold text-xs">
+                            <spam class="bg-yellow-500 text-yellow-900 rounded-md px-3 py-2 ">PHP </spam>
+                            <spam class="bg-fuchsia-500 text-fuchsia-900 rounded-md px-3 py-2 ">JS </spam>
+                            <spam class="bg-sky-500 text-sky-900 rounded-md px-3 py-2 ">CSS </spam>
+                            <spam class="bg-lime-500 text-lime-900 rounded-md px-3 py-2 ">Tailwind </spam>
+                        </div>
+                </div>
+                <p class="leading-6">
+                    Lorem ipsum dolor, sit amet consectetur adipiscign elit.
+                    Alias, quas! Nos voluptatem vero dignissimos nemo magnam iusto nam...
+                </p>
+            </section>
+             <section class="space-y-3 py-6"> 
+                <div class="bg-slate-800 rounded-lg px-4 py-3 flex items-center">
+                    <div class="w-1/5">Foto do projeto</div>
+                    <div class="w-4/5 space-y-3">
+                        <div class="flex gap-3 justify-between">
+                            <h3 class="font-semibold text-xl">
+                                Projeto 3 <spam class="text-sm opacity-50"> (Finalizado em 2025)</spam></h3>
+                        <div class="flex gap-1 font-semibold text-xs">
+                            <spam class="bg-yellow-500 text-yellow-900 rounded-md px-3 py-2 ">PHP </spam>
+                            <spam class="bg-fuchsia-500 text-fuchsia-900 rounded-md px-3 py-2 ">JS </spam>
+                            <spam class="bg-sky-500 text-sky-900 rounded-md px-3 py-2 ">CSS </spam>
+                            <spam class="bg-lime-500 text-lime-900 rounded-md px-3 py-2 ">Tailwind </spam>
+                        </div>
+                </div>
+                <p class="leading-6">
+                    Lorem ipsum dolor, sit amet consectetur adipiscign elit.
+                    Alias, quas! Nos voluptatem vero dignissimos nemo magnam iusto nam...
+                </p>
+            </section>
+        </main>
