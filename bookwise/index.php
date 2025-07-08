d438ab5966b11555613c172c42c44162db1d6ddb
andressa freitas <andressa.freitas@atlastechnol.com>
2025-07-08 07:32:27 -0300

    feat:create_first_routes

--- bookwise/index.php
+++ bookwise/index.php
@@ -1,16 +1,6 @@
 <?php
-echo 'sempre cai no index.php';
 
-$controller = 'index';
-// echo "<pre>";
-// var_dump($_SERVER);
-// echo "</pre>";
+require 'functions.php';
 
-if (isset($_SERVER['PATH_INFO'])) {
-    $controller =str_replace('/','', $_SERVER['PATH_INFO']);
-}
-// isset = verifica se a chave existe no array.
-
-
-require "controllers/{$controller}.controller.php";
+require 'routes.php';
 ?>
