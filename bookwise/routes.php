8e7548c55e85d22c28e93df01673184e580ec70a
andressa freitas <andressa.freitas@atlastechnol.com>
2025-07-08 16:36:06 -0300

    fix:routes

--- bookwise/routes.php
+++ bookwise/routes.php
@@ -1,17 +1,18 @@
 <?php
 
-function carregarController() {
+function carregarController()
+{
 
-$controller = str_replace('/', '', $_SERVER['PATH_INFO']);
-if (!$controller) $controller = 'index';
+    $controller = str_replace('/', '', $_SERVER['PATH_INFO']);
+    if (!$controller) $controller = 'index';
 
-if(!file_exists("controllers/{$controller}.controller.php")) {
-    http_response_code(404);
-    echo "página não existe";
-    die();
-}
+    if (!file_exists("controllers/{$controller}.controller.php")) {
+        http_response_code(404);
+        echo "página não existe";
+        die();
+    }
 
-require "controllers/{$controller}.controller.php";
+    require "controllers/{$controller}.controller.php";
 }
 
 carregarController();
